0b19031dfe06505dd22a9c6d8e62b23ed7cf2021
hippotas98 <nhathuy1202@gmail.com>
2018-10-23 21:31:15 +0700

    add more trees

--- main.cpp
+++ main.cpp
@@ -7,27 +7,31 @@
 #include<string>
 #include<stack>
 using namespace std;
-float width = 2.0;
-float lengthScale = 0.7;
-float leng1 = 2;
+float width = 5.0;
+float lengthScale = 0.75;
+float leng1 = 3;
 float leng2 = 5;
 float leng3 = 2.5;
 float leng4 = 1;
 float angle = 23;
-void DrawLine();
+void drawLine();
 void draw();
-
 
 struct {
 	char f = 'F';
-	string F = "[+++++X]^BDB[-----X]^BDB[++++++X]^B";
-	string G = "[+^[[[--^X]^L]L[+^X]^F[+^X]]]";
+	char k = 'K';
+	//string F = "D[++M[+++++X]][--M[-----X]]BD[--M[-----X]]BD[++M[+++++X]]^B";
+    string F = "D[++M[+++++X]][--C[---X---X]]BD[--M[-----X]]BD[++M[+++++X]]^B";
+
+	string G = "[^^[[[+++^X]]^L[++^X]^K[++^X]]]";
 	//string F = "F+F--F+F";
+
 }Rules;
 
-string axiom = "F";
-string str = axiom;
-string strcrown = axiom;
+string axiom1 = "[F";
+string axiom2 = "F";
+string str = axiom1;
+string strcrown = axiom2;
 void Init()
 {
 	glClearColor(0.0, 0.0, 0.0, 0.0);
@@ -70,67 +74,27 @@ void TurnRight() {
 
 }
 void TurnRightWithCustomizedAngle() {
-
-	glRotatef(-3, 0, 0, 1);
-
+	glRotatef(-1, 0, 0, 1);
 }
-void DrawLine(float scale) {
-
-
-	glLineWidth(width*scale);
-
+void TurnLeftWithCustomizeAngle() {
+    glRotatef(4, 0, 0, 1);
+}
+void DrawLine(float scale, float length, int colorMode) {
+	glLineWidth(width * scale);
 	glBegin(GL_LINES);
-	glColor3f(0.0, 1.0, 0.0);// xanh
+	if(colorMode == 1)
+        glColor3f(0.0, 0.0, 0.0);
+    else glColor3f(0.0, 1.0, 0.0);// xanh
 	glVertex3f(0,0,0);
-	glVertex3f(0,leng1,0);
+	glVertex3f(0,length,0);
 	glEnd();
-	glTranslatef(0, leng1, 0);
+	glTranslatef(0, length, 0);
 
 }
-void DrawBone() {
-
-
-	glLineWidth(width);
-
-	glBegin(GL_LINES);
-	glColor3f(0.0, 1.0, 0.0);// xanh
-	glVertex3f(0, 0, 0);
-	glVertex3f(0, leng2, 0);
-	glEnd();
-	glTranslatef(0, leng2, 0);
-
-}
-void DrawBone1() {
-
-
-	glLineWidth(width);
-
-	glBegin(GL_LINES);
-	glColor3f(0.0, 1.0, 0.0);// xanh
-	glVertex3f(0, 0, 0);
-	glVertex3f(0, leng3, 0);
-	glEnd();
-	glTranslatef(0, leng3, 0);
-
-}
-
-void DrawBone2() {
-
-
-	glLineWidth(width);
-
-	glBegin(GL_LINES);
-	glColor3f(0.0, 1.0, 0.0);// xanh
-	glVertex3f(0, 0, 0);
-	glVertex3f(0, leng4, 0);
-	glEnd();
-	glTranslatef(0, leng4, 0);
-
-}
+// ve dot cay
 void drawDot(){
     float height = 0.1;
     glLineWidth(width);
-
 	glBegin(GL_LINES);
 	glColor3f(1, 1, 1);// trang
 	glVertex3f(0,0,0);
@@ -144,15 +108,12 @@ void push() {
 void pop() {
 	glPopMatrix();
 }
+// ve la cay
 void drawLeaf(void) {
   glColor3f(0.1,0.9,0.1);
   int scale = 20;
   glBegin(GL_POLYGON);
-//  glVertex2d(0.0,0.0);
-//  glVertex2d(1.5/scale,4.5/scale);
-//  glVertex2d(0,9.0/scale);
-//  glVertex2d(-1.5/scale,4.5/scale);
-//  glVertex2d(0.0,0.0);
+
   glVertex2d(0.0,0.0);
   glVertex2d(1.0/scale,1.0/scale);
   glVertex2d(0.8/scale,5.0/scale);
@@ -162,34 +123,25 @@ void drawLeaf(void) {
   glVertex2d(0.0,0.0);
   glEnd();
 }
+// sinh than cay
 void generateBody() {
-
 	string strCurrent = Rules.F;
-//	for (int i = 0; i < str.length(); i++) {
-//		char curent =  str.at(i);
-//		if (curent == Rules.f) {
-//			strCurent += Rules.F;
-//		}
-//		else
-//			strCurent += curent;
-//
-//
-//	}
 	cout << strCurrent;
-
 	str += strCurrent;
-
 }
+// sinh  ngon cay
+
 void generateCrown() {
     leng1 = lengthScale*leng1;
 	leng2 = lengthScale*leng2;
 	leng3 = lengthScale*leng3;
 	leng4 = lengthScale*leng4;
+
 	string strCurrent = "";
 
 	for (int i = 0; i < strcrown.length(); i++) {
 		char current =  strcrown.at(i);
-		if (current == Rules.f) {
+		if (current == Rules.f || current == Rules.k) {
 			strCurrent += Rules.G;
 		}
 		else
@@ -197,7 +149,7 @@ void generateCrown() {
 	}
     strcrown += strCurrent;
 	str += strCurrent;
-	cout <<  strCurrent << strCurrent.length() << "\n";
+	cout <<  strCurrent << " " << strCurrent.length() << "\n";
 }
 void draw() {
 	stack <char> S;
@@ -206,15 +158,24 @@ void draw() {
 		char current = str.at(i);
 		if (current == 'F') {
 			// drawLine
-			DrawLine(1.0);
+			DrawLine(1.0, leng1,0);
 			//glTranslated(x, y + 5, z);
 		}
 		else if (current == 'B') {
 			// turn Left
-			DrawLine(1.0);
+			DrawLine(1.0, leng1,0);
 		}
-		else if(current == 'L') {
-            DrawLine(0.5);
+		else if(current == 'L' || current == 'K') {
+            DrawLine(0.35, leng1,0);
+		}
+		else if (current == 'C') {
+            DrawLine(0.3, leng1 * 0.75,0);
+		}
+		else if(current == 'M') {
+            DrawLine(0.2, leng1*0.2,0);
+		}
+		else if (current == 'H') {
+            DrawLine(1.0, leng1*0.6,1);
 		}
 		else if (current == 'X') {
 			// turn Left
@@ -232,6 +193,9 @@ void draw() {
 			TurnLeft();
 		}
 
+        else if (current == '*') {;
+            TurnLeftWithCustomizeAngle();
+        }
         else if (current == '^') {
             // turn Left with customized angle
             TurnRightWithCustomizedAngle();
@@ -244,12 +208,47 @@ void draw() {
 			pop();
 			S.pop();
 		}
-		else if (current == '!') {
-			if (S.empty()) DrawBone();
-			else if(S.size() == 1) DrawBone1();
-			else if (S.size() == 2) DrawBone2();
-		}
+
 	}
+}
+void generateTree()
+{
+	int treeNum = 4;
+	for(int iter = 0;iter < treeNum;iter++ ){
+		for (int i = 0; i < 2; i++) {
+		generateBody();
+		cout<< "\n";
+		}
+		int branchNum = 3;
+		for(int i = 0;i<branchNum;++i){
+	        for (int j = 0; j < 2; j++) {
+	            generateCrown();
+	            cout<< "\n";
+	        }
+			leng1 = leng1/lengthScale;
+			leng2 = leng2/lengthScale;
+			leng3 = leng3/lengthScale;
+			leng4 = leng4/lengthScale;
+			strcrown = axiom2;
+			if(i != branchNum-1) str +="^^L+";
+		}
+	    cout << str;
+	    leng1 = leng1*lengthScale*2;
+//	    leng2 = leng2*lengthScale;
+//        leng3 = leng3*lengthScale;
+//        leng4 = leng4*lengthScale;
+        string h = "";
+        for(int i = 0;i<iter+1;++i)
+        {
+            h += "H";
+        }
+        string addString = "][++++" + h + "----^^";
+        if(iter!= treeNum - 1) addString += "B";
+	    str += addString ; //tach ve cay lien ke
+	}
+
+
+
 }
 int main(int argc, char** argv)
 {
@@ -257,44 +256,22 @@ int main(int argc, char** argv)
 
 	glutInitDisplayMode(GLUT_SINGLE || GLUT_RGB);
 
-	glutInitWindowSize(800, 800);
+	glutInitWindowSize(1000, 1000);
 
 	glutInitWindowPosition(100, 100);
 
 	glutCreateWindow("BTL");
 
 	Init();
-	for (int i = 0; i < 2; i++) {
-		generateBody();
-		cout<< "\n";
-
-	}
-
+	generateTree();
 	//cout << str << "\n\n";
 	string temp = str;
-	int branchNum = 3;
-	for(int i = 0;i<branchNum;++i){
-        for (int j = 0; j < 2; j++) {
-		generateCrown();
-		cout<< "\n";
-        }
-		leng1 = leng1/lengthScale;
-		leng2 = leng2/lengthScale;
-		leng3 = leng3/lengthScale;
-		leng4 = leng4/lengthScale;
-		strcrown = axiom;
 
-		if(i != branchNum-1) str +="^^L+";
-	}
-    cout << str;
+
+
 	glutReshapeFunc(ReShape);
-
 	glutDisplayFunc(RenderScene);
 	glutMainLoop();
-
 	return 0;
-
-
 }
-
 
